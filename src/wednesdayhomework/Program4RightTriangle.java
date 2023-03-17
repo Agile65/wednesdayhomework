2c72169babfb3fcc6e2ef218fc697a89016e5eac
Agile65 <agiletester41@gmail.com>
2023-03-17 10:06:51 +0000

    My Wednesday homework_week 6

---
+++ src/wednesdayhomework/Program4RightTriangle.java
@@ -0,0 +1,23 @@
+package wednesdayhomework;
+/**
+ * 4. Write a Java Program to print below triangle
+ *
+ * *
+ * **
+ * ***
+ * ****
+ * *****
+ * ******
+ */
+public class Program4RightTriangle
+{
+    public static void main(String[] args)
+    {
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+        System.out.println("*****");
+        System.out.println("******");
+    }
+}
---
+++ src/wednesdayhomework/Program6Flag.java
@@ -0,0 +1,25 @@
+package wednesdayhomework;
+/**
+ * 6. Write a Java program to print an American flag on the screen.
+ */
+public class Program6Flag
+{
+    public static void main (String [] args)
+    {
+        System.out.println("* * * * * * ===================================");
+        System.out.println(" * * * * *  ===================================");
+        System.out.println("* * * * * * ===================================");
+        System.out.println(" * * * * *  ===================================");
+        System.out.println("* * * * * * ===================================");
+        System.out.println(" * * * * *  ===================================");
+        System.out.println("* * * * * * ===================================");
+        System.out.println(" * * * * *  ===================================");
+        System.out.println("* * * * * * ===================================");
+        System.out.println("===============================================");
+        System.out.println("===============================================");
+        System.out.println("===============================================");
+        System.out.println("===============================================");
+        System.out.println("===============================================");
+        System.out.println("===============================================");
+    }
+}
